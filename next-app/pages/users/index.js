70461d96e27906ab170ca3516b54e5c6c7e262b5
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-06-28 14:27:10 +0330

    fix: The Dynamic Route in nextJs is add

---
+++ next-app/pages/users/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UsersList() {
+  return (
+    <>
+        <h1>UsersList</h1>
+        <p>user 1</p>
+        <p>user 2</p>
+    </>
+  )
+}
+
+export default UsersList
