f47965bc187ae3295196b36a78c7844b226c9972
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-06-29 10:21:05 +0330

    Fix: The Link component in netJs is add.

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,11 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
      <h1>Golabi.ir</h1>
+     <ul>
+        <li>
+            <Link href="/courses" >Go to Courses Page</Link>
+        </li>
+        <li>
+            <Link href="/users">Go to Users Page</Link>
+        </li>
+
+     </ul>
     </div>
   )
 }
