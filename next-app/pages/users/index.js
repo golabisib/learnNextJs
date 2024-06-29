f0ddd59671e0dc30166f33646c8c8f4ea3c49b2d
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-06-29 10:35:40 +0330

    fix: The Link for Dynamic Routes is add.

--- next-app/pages/users/index.js
+++ next-app/pages/users/index.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+
+const users = [
+  { id: 1, name: 1 },
+  { id: 2, name: 2 },
+  { id: 3, name: 3 },
+  { id: 4, name: 4 },
+];
 
 function UsersList() {
   return (
     <>
-        <h1>UsersList</h1>
-        <p>user 1</p>
-        <p>user 2</p>
+    <h1>Users List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link href={`/users/${user.id}`}>user {user.name}</Link>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default UsersList
+export default UsersList;
