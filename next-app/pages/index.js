217b3115f3aa2dda0efc10b706dd6bde391a363d
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-06-29 10:44:04 +0330

    Fix: The UseRouter routing is add.

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -2,8 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+    const router = useRouter();
+
+    const clickHandler = () => {
+        // Logging process successful
+        router.push("/users")
+    }
   return (
     <div className={styles.container}>
      <h1>Golabi.ir</h1>
@@ -13,6 +20,9 @@ export default function Home() {
         </li>
         <li>
             <Link href="/users">Go to Users Page</Link>
+        </li>
+        <li>
+            <button onClick={clickHandler}>Log in</button>
         </li>
 
      </ul>
