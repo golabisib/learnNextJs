369d2c8fd79fd82b0bd7eb75e6118e6a8cb17412
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-07-21 22:21:05 +0330

    Fix: The getStaticPaths function is add.

--- pre-rendering/pages/users/[userId].js
+++ pre-rendering/pages/users/[userId].js
@@ -1,14 +1,35 @@
 import React from 'react'
 
-function UserDetail() {
+function UserDetail({ data }) {
   return (
     <div>
-        UserDetail
+        <h1>UserDetail</h1>{" "}
+        <h3>{data.name}</h3>
+        <h3>{data.email}</h3>
+        <h3>{data.address.city}</h3>
     </div>
   )
 }
 
 export default UserDetail
+
+export async function getStaticPaths(){
+    return {
+        paths : [
+            {params: {userId: "1"}},
+            {params: {userId: "2"}},
+            {params: {userId: "3"}},
+            {params: {userId: "4"}},
+            {params: {userId: "5"}},
+            {params: {userId: "6"}},
+            {params: {userId: "7"}},
+            {params: {userId: "8"}},
+            {params: {userId: "9"}},
+            {params: {userId: "10"}},
+        ],
+        fallback: false,
+    }
+}
 
 export async function getStaticProps(context) {
     const {params} = context;
