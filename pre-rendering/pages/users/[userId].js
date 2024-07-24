1e482b7d2b697781cf6187f2b92ad3208d1329a8
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-07-25 00:17:04 +0330

    Fix: The Fallback: true; is add.

--- pre-rendering/pages/users/[userId].js
+++ pre-rendering/pages/users/[userId].js
@@ -1,6 +1,12 @@
+import { notFound } from "next/navigation";
+import { useRouter } from "next/router";
 import React from "react";
 
 function UserDetail({ data }) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h2>fallback Page</h2>;
+  }
   return (
     <div>
       <h1>UserDetail</h1> <h3>{data.name}</h3>
@@ -13,12 +19,16 @@ function UserDetail({ data }) {
 export default UserDetail;
 
 export async function getStaticPaths() {
-  const res = await fetch("http://localhost:4000/posts");
+  const res = await fetch("http://localhost:4000/users");
   const data = await res.json();
-  const paths = data.map((user) => ({ params: { userId: user.id } }));
+  const usersData = data.slice(0, 4);
+  const paths = usersData.map((user) => ({
+    params: { userId: user.id.toString() },
+  }));
+
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 }
 
@@ -26,6 +36,11 @@ export async function getStaticProps(context) {
   const { params } = context;
   const res = await fetch(`http://localhost:4000/users/${params.userId}`);
   const data = await res.json();
+  if (!data.name) {
+    return {
+      notFound: true,
+    };
+  }
   return {
     props: { data },
   };
