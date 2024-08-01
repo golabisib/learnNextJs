2351a4a3b97af301be34c70d61684a317015ff5f
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-08-01 21:35:00 +0330

    Fix: The useSWR hook and SWR is add.

--- pre-rendering/pages/todos/index.js
+++ pre-rendering/pages/todos/index.js
@@ -1,17 +1,23 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+
+import useSWR from "swr";
+
 
 function ToDos() {
-  const [todos, setTodos] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:4000/todos")
-      .then((res) => res.json())
-      .then((data) => setTodos(data));
-  }, []);
+  //   const [todos, setTodos] = useState([]);
+  //   useEffect(() => {
+  //     fetch("http://localhost:4000/todos")
+  //       .then((res) => res.json())
+  //       .then((data) => setTodos(data));
+  //   }, []);
+
+  const {data, error} = useSWR("http://localhost:4000/todos", (url) => fetch(url).then((res) => res.json()));
+  console.log({ data, error})
 
   return (
     <div>
-      {todos.length ? (
-        todos.map((todo) => <h3 key={todo.id}>{todo.title}</h3>)
+      {data ? (
+        data.map((todo) => <h3 key={todo.id}>{todo.title}</h3>)
       ) : (
         <h1>Loading...</h1>
       )}
